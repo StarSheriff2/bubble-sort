8bca8247c38788812fc23b9ed6294889863272af
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-01 14:21:30 -0600

    complete the bubble-sort-by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,4 +16,29 @@ def bubble_sort(array)
   return array
 end
 
-p bubble_sort([5,8,2,4])
+
+def bubble_sort_by(array)
+  
+  swapped = true
+
+  while swapped
+    swapped = false
+
+    for i in 0...array.length-1
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+  end
+  
+  return array
+
+
+
+end
+
+p bubble_sort([20,19,15,9,8,7,6,5,4])
+
+p bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}
+
