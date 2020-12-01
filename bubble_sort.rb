27f0c31521e9a607ff05fe0091effeea14970d33
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-01 15:03:57 -0600

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,21 +1,19 @@
 def bubble_sort(array)
-
   swapped = true
 
   while swapped
     swapped = false
 
-    0.upto(array.length-2) do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
+    0.upto(array.length - 2) do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
     end
   end
   
-  return array
+  array
 end
-
 
 def bubble_sort_by(array)
   
@@ -24,21 +22,17 @@ def bubble_sort_by(array)
   while swapped
     swapped = false
 
-    0.upto(array.length-2) do |i|
+    0.upto(array.length - 2) do |i|
       if yield(array[i], array[i + 1]).positive?
-        array[i], array[i+1] = array[i+1], array[i]
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
     end
   end
   
-  return array
-
-
+  array
 
 end
 
-p bubble_sort([20,19,15,9,8,7,6,5,4])
-
-p bubble_sort_by(["elephant","tiger","bat"]) {|left,right| left.length - right.length}
-
+p bubble_sort([20, 19, 15, 9, 8, 7, 6, 5, 4])
+p bubble_sort_by(%w[hi hey hello]) {|left,right| left.length - right.length}
